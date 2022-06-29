c3c9183a1f2a1116b2ce2e92f724b8ab2ea86b72
thetechrat <ogunyemialameen@gmail.com>
2022-06-29 08:29:10 -0700

    destination page first commit

--- starter-code/Js/index.js
+++ starter-code/Js/index.js
@@ -5,3 +5,23 @@ hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 });
+
+// DESTINATION SWITCH
+const dMoon = document.querySelector(".p-info2");
+const destMoon = document.getElementById("moon");
+
+function switchMars() {
+  destMoon.innerHTML = document.getElementById("mars").innerHTML;
+}
+
+function switchEur() {
+  destMoon.innerHTML = document.getElementById("europa").innerHTML;
+}
+
+function switchTit() {
+  destMoon.innerHTML = document.getElementById("titan").innerHTML;
+}
+
+function switchMoon() {
+  destMoon.innerHTML = dMoon.innerHTML;
+}
